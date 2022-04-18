ea12028452bcd679af6e02c3db4f609f744448b1
Francisco Depascuali <depa.developer@gmail.com>
2022-04-18 13:00:27 -0300

    Add Sessions endpoint

---
+++ Sources/ConfigureID-iOS-SDK/Models/Internal/Request+Customers.swift
@@ -0,0 +1,61 @@
+//
+//  File.swift
+//  
+//
+//  Created by Francisco D. on 18/04/2022.
+//
+
+import Foundation
+
+extension Request {
+    
+    struct Customer {
+        
+        static func products(customerId: String, apiKey: String, workflow: String) -> Request {
+            var components = URLComponents()
+            // TODO: allow to modify this
+            components.scheme = "https"
+            // TODO: allow to modify this
+            components.host = "headless.fluidconfigure.com"
+            components.path = "/customers/\(customerId)/products"
+
+            components.queryItems = [
+                URLQueryItem(name: "api-key", value: apiKey),
+                URLQueryItem(name: "workflow", value: workflow)
+            ]
+            
+            return Request(method: .GET, urlComponents: components)
+        }
+        
+        static func productData(customerId: String, productId: String, apiKey: String, workflow: String) -> Request {
+            var components = URLComponents()
+            // TODO: allow to modify this
+            components.scheme = "https"
+            // TODO: allow to modify this
+            components.host = "headless.fluidconfigure.com"
+            components.path = "/customers/\(customerId)/products/\(productId)"
+
+            components.queryItems = [
+                URLQueryItem(name: "api-key", value: apiKey),
+                URLQueryItem(name: "workflow", value: workflow)
+            ]
+            
+            return Request(method: .GET, urlComponents: components)
+        }
+        
+        static func findByVendorId(customerId: String, apiKey: String, vendorId: String, workflow: String) -> Request {
+            var components = URLComponents()
+            // TODO: allow to modify this
+            components.scheme = "https"
+            // TODO: allow to modify this
+            components.host = "headless.fluidconfigure.com"
+            components.path = "/customers/\(customerId)/products/find-by-vendor-id/\(vendorId)"
+            components.queryItems = [
+                URLQueryItem(name: "api-key", value: apiKey),
+                URLQueryItem(name: "workflow", value: workflow)
+            ]
+        
+            return Request(method: .GET, urlComponents: components)
+        }
+    }
+}
---
+++ Sources/ConfigureID-iOS-SDK/Models/Internal/Request+Sessions.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Francisco D. on 18/04/2022.
+//
+
+import Foundation
+
+extension Request {
+    
+    struct Sessions {
+        
+        // TODO: is workflow needed?
+        static func session(sessionId: String, apiKey: String, workflow: String) -> Request {
+            var components = URLComponents()
+            // TODO: allow to modify this
+            components.scheme = "https"
+            // TODO: allow to modify this
+            components.host = "headless.fluidconfigure.com"
+            components.path = "/sessions/\(sessionId)"
+
+            components.queryItems = [
+                URLQueryItem(name: "api-key", value: apiKey),
+                URLQueryItem(name: "workflow", value: workflow)
+            ]
+            
+            return Request(method: .GET, urlComponents: components)
+        }
+        
+    }
+}
