de4cfc4ea29885bb41d0260082bdf5e8beead725
Francisco Depascuali <depa.developer@gmail.com>
2022-04-21 14:12:47 -0300

    Add example project

---
+++ ConfigureID-iOS-SDK-Demo/Shared/ConfigureID_iOS_SDK_DemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ConfigureID_iOS_SDK_DemoApp.swift
+//  Shared
+//
+//  Created by Francisco D. on 21/04/2022.
+//
+
+import SwiftUI
+import ConfigureID_iOS_SDK
+
+@main
+struct ConfigureID_iOS_SDK_DemoApp: App {
+    var body: some Scene {
+        return WindowGroup {
+            ContentView()
+        }
+    }
+}
